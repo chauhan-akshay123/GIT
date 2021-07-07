21d68663249d69bf782b93858dcac359a819a098
Akshay Chauhan <chauhanakshay395@gmail.com>
2021-07-07 18:36:06 +0530

    added multiply

---
+++ multiply.cpp
@@ -0,0 +1,12 @@
+# include <iostream>
+using namespace std;
+float multiply(int a,int b){
+    return a*b;
+}
+int main(){
+int a = 2;
+int b = 4;
+cout<<"THE PRODUCT : "<<multiply(a,b)<<endl;
+
+    return 0;
+}
